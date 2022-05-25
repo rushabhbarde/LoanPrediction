f066c65f2fe51ecbe8d36e119229c7e0bbb54a32
rushabhbarde <88505500+rushabhbarde@users.noreply.github.com>
2022-05-25 22:43:00 +0530

    Add files via upload

--- static/email.php
+++ static/email.php
@@ -1,25 +1,16 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-
-
-    $email_from = 'rushabhbarde@gmail.com';
-
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name.\n".
-                        "User Email: $visitor_email.\n".
-                            "User Message: $message.\n"; 
-                            
-                            
-    $to = "rushabhbarde@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-
-    $headers .="Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "rushabhbarde@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:contact.html");
 ?>
